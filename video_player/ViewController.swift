4d84502d16cb3236cbbc25036173c955ec54a58e
Temiloluwa <91268094+Ytemiloluwa@users.noreply.github.com>
2022-09-03 05:47:48 +0100

    Initial Commit

---
+++ video_player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  video_player
+//
+//  Created by Temiloluwa on 03/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
