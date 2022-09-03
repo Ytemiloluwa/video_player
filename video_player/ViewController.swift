fc3f4eb48ffa08ba9dd5dd60d80b3711e965bb67
Temiloluwa <91268094+Ytemiloluwa@users.noreply.github.com>
2022-09-03 06:17:42 +0100

    PlayController

--- video_player/ViewController.swift
+++ video_player/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Foundation
+import AVKit
 
 class ViewController: UIViewController {
 
@@ -13,7 +15,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(<#T##animated: Bool##Bool#>)
+        
+        playVideo()
+    }
+    private func playVideo() {
+        guard let path = Bundle.main.path(forResource: "Introduction to the program", ofType: "mp4") else { debugPrint("Mp4 not found "); return }
+        
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        
+        let PlayerController = AVPlayerViewController()
+        PlayerController.player = player
+        
+    }
 
 }
 
